b84e3829fb5f826079c9c5b58a2ab82fdb0ef755
dojinyou <djyou128@gmail.com>
2022-06-08 23:46:20 +0900

    :sparkles: init: sprint security project d1

---
+++ part1/D1/ss-quickstart/src/main/java/com/dojinyou/devcourse/sprintsecurityquickstart/SsQuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.dojinyou.devcourse.sprintsecurityquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsQuickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsQuickstartApplication.class, args);
+	}
+
+}
---
+++ part1/D1/ss-quickstart/src/test/java/com/dojinyou/devcourse/sprintsecurityquickstart/SsQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojinyou.devcourse.sprintsecurityquickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsQuickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
